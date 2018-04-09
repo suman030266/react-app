2d9d8164174889f54cb3e78caa37c6355071b577
suman <suman@gomeplus.com>
2018-04-09 22:19:55 +0800

    增加头部动画

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -2,11 +2,14 @@ import React,{Component} from 'react';
 import './index.less';
 import HomeHeader from './HomeHeader';
 export default class Home extends Component{
+  selectCurrentLesson = (value) =>{
+    console.log(value);
+  }
   render(){
     return (
       <div>
         home
-        <HomeHeader />
+        <HomeHeader selectCurrentLesson={this.selectCurrentLesson} />
       </div>
     )
   }
--- src/containers/Home/HomeHeader.js
+++ src/containers/Home/HomeHeader.js
@@ -1,5 +1,18 @@
 import React,{Component} from 'react';
 import logo from '../../common/images/logo.jpg';
+import Transition from 'react-transition-group/Transition';
+
+const duration = 300;
+const defaultStyle = {
+  transtion : `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+  display: 'none'
+};
+const transitionStyle = {
+  entering: {opacity: 0},
+  entered: {opacity: 1}
+};
+
 export default class HomeHeader extends Component{
   constructor(){
     super();
@@ -21,11 +34,30 @@ export default class HomeHeader extends Component{
             {this.state.isShow ? <i className="iconfont icon-guanbi"></i> : <i className="iconfont icon-liebiao"></i>}
           </div>
         </div>
-        <ul className="header-menu">
-          <li>全部课程</li>
-          <li>react</li>
-          <li>vue</li>
-        </ul>
+        <Transition in={this.state.isShow} timeout={duration}
+          onEnter={(node) =>{
+            node.style.display = 'block';
+          }}
+          onExited={(node) =>{
+            node.style.display = 'none';
+          }}
+        >
+          {(state) => (
+            <ul className="header-menu" style={{
+                ...defaultStyle,
+                ...transitionStyle[state]
+              }}
+              onClick={(e)=>{
+                this.props.selectCurrentLesson(e.target.dataset.type);
+                this.changeShow();
+              }}
+            >
+              <li data-type="all">全部课程</li>
+              <li data-type="react">react</li>
+              <li data-type="vue">vue</li>
+            </ul>
+          )}
+        </Transition>
       </div>
     )
   }
